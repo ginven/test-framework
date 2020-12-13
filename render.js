44dd80a1b844ed0b75fc9915d74d1ad65ee263cc
Gintare Venckiene <gintare.venckiene@gmail.com>
2020-12-13 18:30:14 +0200

    Added new sample web project and tests to it

--- render.js
+++ render.js
@@ -4,10 +4,17 @@ const { JSDOM } = jsdom;
 
 const render = async (filename) => {
 	const filePath = path.join(process.cwd(), filename);
+ 	
  	const dom = await JSDOM.fromFile(filePath, {
  		runScripts: 'dangerously',
  		resources: 'usable'
  	});
+
+ 	return new Promise((resolve, reject) => {
+ 		dom.window.document.addEventListener('DOMContentLoaded', () => {
+ 			resolve(dom);
+ 		})
+ 	})
 
  	return dom;
 }
---
+++ samplewebproject/test/app.test.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+
+it('has a text input', async () => {
+	const dom = await render('index.html');
+
+	const input = dom.window.document.querySelector('input');
+
+	assert(input);
+	
+});
+
+it('shows success message when email is valid', async () => {
+	const dom = await render('index.html');
+	const input = dom.window.document.querySelector('input');
+	input.value = 'test@test.com';
+
+	dom.window.document.querySelector('form').dispatchEvent(new dom.window.Event('submit'));
+
+	const h1 = dom.window.document.querySelector('h1');
+	assert.strictEqual(h1.innerHTML, 'Looks good');
+});
+
+it('shows fail message when email is invalid', async () => {
+	const dom = await render('index.html');
+	const input = dom.window.document.querySelector('input');
+	input.value = 'testtest.com';
+
+	dom.window.document.querySelector('form').dispatchEvent(new dom.window.Event('submit'));
+
+	const h1 = dom.window.document.querySelector('h1');
+	assert.strictEqual(h1.innerHTML, 'Invalid email');
+});
